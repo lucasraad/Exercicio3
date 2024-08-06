b5d3b7eb3f1e328993feb22b9f674f529ea15229
lucas silva raad <lucasraadr@gmail.com>
2024-08-06 12:18:04 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Threading;
+
+namespace Exercicio3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome1, nome2;
+            int idade1, idade2;
+            double media;
+            string[] vet;
+
+            vet = Console.ReadLine().Split(' ');
+            nome1 = vet[0];
+            idade1 = int.Parse(vet[1]);
+
+            vet = Console.ReadLine().Split(' ');
+            nome2 = vet[0];
+            idade2 = int.Parse(vet[1]);
+
+            media = (idade1 + idade2) / 2.0;
+
+            Console.WriteLine("A idade media de " + nome1 + " e " + nome2 + " é de "
+                + media.ToString("F1", CultureInfo.InvariantCulture));
+            Console.ReadLine();
+
+        }
+    }
+}
